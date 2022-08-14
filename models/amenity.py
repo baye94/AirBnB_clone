7bddeb4fd5c383e2a6184e8b0fe5f8e5bf6c4127
yeonoel <yeopevroguinoel@gmail.com>
2022-08-14 02:18:01 +0000

    Up to date

--- models/amenity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Define Amenity class which inherits the BaseModel class."""
-
-from models.base_model import BaseModel
-
-class Amenity(BaseModel):
-    '''Representation of Amenity Class Definition.'''
-
-    name = ''
--- models/state.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Define State class which inherits the BaseModel class."""
-
-from models.base_model import BaseModel
-
-class State(BaseModel):
-    '''Representation of State Class.'''
-
-    name = ''
